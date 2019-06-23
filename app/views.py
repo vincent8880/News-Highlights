34a8def18b8cf96ab8c4097ebc89ccb2867be2d3
vincent <ododovincent54@gmail.com>
2019-06-24 00:28:43 +0300

    updated my sources.html

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from flask import render_template
 from app import app
 from .request import get_news
-from.request import get_sources
+from .request import get_sources
+from .request import get_articles 
 
 
 
 # Views
-@app.route('/')
+@app.route('/index')
 def index():
 
     '''
@@ -18,10 +19,20 @@ def index():
     print(general_news)
     title = 'Top Headlines'
     return render_template('index.html', name='Top Headlines', title = title,news = general_news)
-@app.route('/article/')
+@app.route('/')
 def article():
 
     sources = get_sources('en')
-    print(sources)
+    
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,sources = sources)
+    return render_template('article.html', title = title,sources = sources)
+@app.route('/sources/<source_id>&<int:per_page>')
+def articles(source_id,per_page):
+    '''
+    Function that returns articles based on their sources
+    '''
+    # print(source_id)
+    # per_page = 40
+    news_source = get_articles(source_id,per_page)
+    title = f'{source_id} | All Articles'
+    return render_template('sources.html', title = title, name = source_id, news = news_source)
